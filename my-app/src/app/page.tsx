e3a6436d87f10aafc3803c336400df0f7110482c
YenLun <yenlunliu@gmail.com>
2025-06-24 09:52:15 -0700

    add restaurants cards

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "./components/header/Header";
 import Hero from "./components/hero/Hero";
+import RestaurantsPage from "./restaurants/page";
 
 export default function Home() {
     return (
         <>
             <Header />
             <Hero />
+            <RestaurantsPage />
         </>
     );
 }
