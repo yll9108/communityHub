ec879e325c6ba0174fed3eb4ce730e6f3ffa95b9
YenLun <yenlunliu@gmail.com>
2025-06-24 23:44:53 -0700

    adjust category section

--- my-app/src/app/components/hero/Hero.tsx
+++ my-app/src/app/components/hero/Hero.tsx
@@ -6,17 +6,19 @@ import Card from "../shared/Card";
 function Hero() {
     return (
         <>
-            <section className="hero bg-gradient-to-br from-blue-50 to-indigo-100 min-h-2/3">
-                <div className="hero-content text-center">
-                    <div className="max-w-3/4">
-                        <Title />
-                        <Search />
+            <div className="h-screen">
+                <section className="hero bg-gradient-to-br from-blue-50 to-indigo-100 h-96">
+                    <div className="hero-content text-center">
+                        <div className="max-w-3/4">
+                            <Title />
+                            <Search />
+                        </div>
                     </div>
-                </div>
-            </section>
-            <section className="flex">
-                <Card />
-            </section>
+                </section>
+                <section className="flex justify-center">
+                    <Card />
+                </section>
+            </div>
         </>
     );
 }
--- my-app/src/app/components/shared/Card.tsx
+++ my-app/src/app/components/shared/Card.tsx
@@ -9,7 +9,7 @@ function Card() {
                 return (
                     <div
                         key={category.title}
-                        className="card bg-base-100 w-96 shadow-sm"
+                        className="card bg-base-100 w-96 shadow-sm mt-8 mr-6"
                     >
                         <figure className="px-10 pt-10">
                             <Icon className="h-12 w-12 text-orange-500 mx-auto mb-4 group-hover:scale-110 transition-transform" />
